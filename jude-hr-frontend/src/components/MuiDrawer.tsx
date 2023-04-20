adb02aa9f6a186189ed7021318378b93ca701af3
judeNdagga <ndaggajude88@gmail.com>
2023-04-20 14:43:04 +0300

    Update MuiDrawer.tsx

--- jude-hr-frontend/src/components/MuiDrawer.tsx
+++ jude-hr-frontend/src/components/MuiDrawer.tsx
@@ -1,7 +1,7 @@
 import { Drawer, Box, Typography, IconButton } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu'
 import { useState } from "react";
-
+import { Outlet, Link } from "react-router-dom";
 export const MuiDrawer = () => {
 
     const [isDrawerOpen, setIsDrawerOpen] = useState(true)
@@ -24,6 +24,18 @@ export const MuiDrawer = () => {
                 <Typography variant='h6' component='div'>
                     HR Software
                 </Typography>
+
+        <ul>
+          <li>
+            <Link to="Holidays">Holidays</Link>
+          </li>
+          <li>
+            <Link to="/Onboard">Onboard</Link>
+          </li>
+          <li>
+            <Link to="/Profiles">Profiles</Link>
+          </li>
+        </ul>
             </Box>
         </Drawer>
         </>)
